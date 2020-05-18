fa79efe0aecee802f2df6c82bcc588c624378130
SilviaVec <44430562+SilviaVec@users.noreply.github.com>
2020-05-18 21:09:20 +0200

    Add files via upload

--- src/s5_test_new.py
+++ src/s5_test_new.py
@@ -4,6 +4,8 @@ import simplejson
 import numpy as np
 import functools
 import pickle
+
+
 if True:  # Include project path
     import sys
     import os
@@ -13,6 +15,24 @@ if True:  # Include project path
 
     from utils.lib_classifier import ClassifierOnlineTest
     from utils.lib_skeletons_io import load_skeleton_data
+
+    
+def remove_skeletons_with_few_joints(skeletons):                              
+    ''' Remove bad skeletons before sending to the tracker '''
+    good_skeletons = []
+    px = skeletons[3:3+13*3:3]
+    py = skeletons[4:3+13*3:3]
+    pz = skeletons[5:3+13*3:3]
+    num_valid_joints = len([x for x in px if x != 0])
+    num_leg_joints = len([x for x in px[-6:] if x != 0])
+    total_size = max(py) - min(py)
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # IF JOINTS ARE MISSING, TRY CHANGING THESE VALUES:
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if num_valid_joints >= 5 and total_size >= 0.1 and num_leg_joints >= 0:
+        # add this skeleton only when all requirements are satisfied
+        good_skeletons.append(skeletons)
+    return good_skeletons
 
 
 def getXYZandName(ll, person_id = ' 0'):                                            
@@ -49,8 +69,9 @@ def getXYZandName(ll, person_id = ' 0'):
         numero=coords[(4*i)-1]
         coords.remove(numero)
 
-    coordsNew=[]
     ordine=[1,0,9,10,11,3,4,5,12,13,14,6,7,8,16,15,18,17]
+
+    coordsNew= []
 
     indici= range(0,18)
     for i in indici:
@@ -59,15 +80,17 @@ def getXYZandName(ll, person_id = ' 0'):
         coordsNew.append(coords[ordine[i]*3+2])
     for element in coordsNew:
         stringaOut = stringaOut + ", " + (str(element))
-
+    
     univocName=ll.get("univTime")
     return coordsNew, univocName
+
 
 
 
 WINDOW_SIZE = 0.5
 ROOT = os.path.dirname(os.path.abspath(__file__))+"/../"
 sys.path.append(ROOT)
+
 
 ROOTOutput = ROOT + "output/"
 if not os.path.exists(ROOTOutput):
@@ -79,15 +102,16 @@ filepath = ROOT+"data_test/"
 
 
 iniziale = 809
-finale = 814
+finale = 881
 contaFrame = 0
+dict_id2skeleton = {}
 
 finale_contenuto = ""
 
 while (iniziale < finale+1):
     name=str(iniziale)
-    nomeDaScrivere = str(contaFrame)
-    contaFrame = contaFrame+1           
+    nomeDaScrivere = str(contaFrame) 
+    contaFrame = contaFrame+1         
     while(len(name)<8):
         name = "0" + name  #SERVE PERCHE LE FOTO SI CHIAMANO 00076
     fileDaAprire = filepath + "body3DScene_" + name +".json"
@@ -98,9 +122,15 @@ while (iniziale < finale+1):
     if (coordsxyz== None):
         coordsxyz = ', 0'* (54) 
 
+    coordsxyz_np = np.asarray(coordsxyz)
+
+    coordsxyz_np = remove_skeletons_with_few_joints(coordsxyz_np)
+
     create_classifier = ClassifierOnlineTest(SRC_MODEL_PATH, classes, WINDOW_SIZE, 0)
     create_classifier.reset()
-    create_classifier.predict(coordsxyz)
+    dict_id2skeleton = {1: coordsxyz_np}
+    
+    id2label = create_classifier.predict(coordsxyz_np) 
 
     stringaOut =''
     finale_contenuto=''
